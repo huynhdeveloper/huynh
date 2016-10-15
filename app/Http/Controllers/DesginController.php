6b2394f5bd5e5b0b92a0bb2def593f378f0dbf98
huynhdeveloper <huynhcntt2020@gmail.com>
2016-10-15 14:47:17 +0700

    Add files via upload

---
+++ app/Http/Controllers/DesginController.php
@@ -0,0 +1,242 @@
+<?php
+  
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Json;
+use App\Models\Account;
+
+use DB;
+  
+  
+class DesginController extends Controller{
+    
+    public function apiCategary(){
+        return DB::select("select * from categary");
+    }
+    
+    public function apiSub(){
+        return DB::select("select * from sub");
+    }
+    
+    public function apiAccount(){
+        return DB::select("select * from account");
+    }
+    
+    public function apiProduct(){
+        return DB::select("select * from product");
+    }
+    
+    public function apiProductCate(){
+        return DB::select('select product.name, product.dis, product.img from show_product,categary,product WHERE 
+
+                            show_product.product = product.id and
+
+                            show_product.cate = categary.id and 
+                            
+                            show_product.hide = false
+
+                            GROUP BY show_product.id');
+    }
+    
+    public function apiProductSub(){
+        return DB::select('select sub.id, sub.name from sub');
+    }
+    
+    public function apiProductCateAndSub($cate){
+        return DB::select('select sub.id, sub.name from sub where cate = '.$cate);
+    }
+    
+    public function apiResgister($account){
+        $array = array(
+            Account::ACCOUNT_USER => $account->getUserName()."",
+            Account::ACCOUNT_PASS => $account->getPassword()."",
+            Account::ACCOUNT_PHONE => $account->getPhone()."",
+            Account::ACCOUNT_SEX => $account->getSex()."",
+            Account::ACCOUNT_ADDRESS => $account->getAddress().""
+        );
+        
+        $count = DB::table(Account::TABLE_ACCOUNT)->where(Account::ACCOUNT_USER, '=' , $account->getUserName())->get();
+        
+        if(count($count) == 0){
+            DB::table(Account::TABLE_ACCOUNT)->insert($array);
+            return DB::table(Account::TABLE_ACCOUNT)->select()->where(Account::ACCOUNT_USER, '=' , $account->getUserName())->get();
+            
+        }else{
+            return array();
+        }
+    }
+    
+    public function apiLogIn($account){
+        
+        $count = DB::table(Account::TABLE_ACCOUNT)->where(Account::ACCOUNT_USER, '=' , $account->getUserName())->get();
+        if(count($count) == 0){
+            return "fail";
+        }else{
+            return DB::table(Account::TABLE_ACCOUNT)->select()->where(Account::ACCOUNT_USER, '=' , $account->getUserName())->get();
+        }
+    }
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public function showAll(){
+        
+        $product = DesginController::apiProduct();
+        
+        return view('show.showAll')->with('product',json_encode($product));
+        
+    }
+    
+    public function showCate(){
+        
+        $product = DesginController::apiProductCate();
+        
+        return view('show.showCate')->with('product',json_encode($product));
+        
+    }
+    
+    public function showSub(){
+        
+        $product = DesginController::apiProductSub();
+        
+        return view('show.showSub')->with('product',json_encode($product));
+        
+    }
+    
+    public function showCateAndSub(Request $request){
+        
+        if($request->input("cate") == ""){
+            return view('show.showSub')->with('product',json_encode(array()));
+        }
+        $cate = $request->input("cate");
+        
+        $product = DesginController::apiProductCateAndSub($cate);
+        
+        return view('show.showCateAndSub')->with('product',json_encode($product));
+        
+    }
+    
+    public function showIndex(){
+        
+        $cate = DesginController::apiCategary();
+        
+        $sub = DesginController::apiSub();
+        
+        $account = DesginController::apiAccount();
+        
+        return view('index')->with('cate',json_encode($cate))->with('sub',json_encode($sub))->with('account',json_encode($account));
+        
+    }
+    
+    
+    public function showRegister(Request $request){
+        
+        if($request->input(Account::ACCOUNT_USER) == ""){
+            return view('show.showRegister')->with('message','fail deficient username');
+        }
+        
+        if($request->input(Account::ACCOUNT_PASS) == ""){
+            return view('show.showRegister')->with('message','fail deficient password');
+        }
+        
+        if($request->input(Account::ACCOUNT_PHONE) == ""){
+            return view('show.showRegister')->with('message','fail deficient phone');
+        }
+        
+        if($request->input(Account::ACCOUNT_SEX) == ""){
+            return view('show.showRegister')->with('message','fail deficient sex');
+        }
+        
+        if($request->input(Account::ACCOUNT_ADDRESS) == ""){
+            return view('show.showRegister')->with('message','fail deficient address');
+        }
+        
+        $account = new Account();
+        
+        $account->setUserName($request->input(Account::ACCOUNT_USER));
+        
+        $account->setPassword($request->input(Account::ACCOUNT_PASS));
+        
+        $account->setPhone($request->input(Account::ACCOUNT_PHONE));
+        
+        $account->setSex($request->input(Account::ACCOUNT_SEX));
+        
+        $account->setAddress($request->input(Account::ACCOUNT_ADDRESS));
+        
+        $user = DesginController::apiResgister($account);
+        
+        return view('show.showRegister')->with('message',json_encode($user));
+        
+    }
+    
+    public function showLogin(Request $request){
+        
+        if($request->input(Account::ACCOUNT_USER) == ""){
+            return view('show.showLogin')->with('message','fail deficient username');
+        }
+        
+        if($request->input(Account::ACCOUNT_PASS) == ""){
+            return view('show.showLogin')->with('message','fail deficient password');
+        }
+        
+        $account = new Account();
+        
+        $account->setUserName($request->input(Account::ACCOUNT_USER));
+        
+        $account->setPassword($request->input(Account::ACCOUNT_PASS));
+        
+        $login = DesginController::apiLogIn($account);
+        
+        return view('show.showLogin')->with('message',json_encode($login));   
+    }
+    
+    public function showDisable(Request $request){
+        if($request->input(Account::ACCOUNT_USER) == ""){
+            return view('show.showDisable')->with('message','fail deficient username');
+        }
+        $dis = DB::select("select * from account where username like '".$request->input(Account::ACCOUNT_USER)."'");
+        
+        foreach($dis as $i => $item){
+            
+            $item = (array)$item;
+            
+            $dis = $item['dis'];
+        }
+        
+        if($dis == 0){
+            
+            $array_where = array(Account::ACCOUNT_USER => $request->input(Account::ACCOUNT_USER));
+        
+            $array_update = array(Account::ACCOUNT_DIS => 1);
+        
+            DB::table(Account::TABLE_ACCOUNT)->where($array_where)->update($array_update);
+            
+            return view('show.showDisable')->with('message','clock successfuly');
+        }else{
+            $array_where = array(Account::ACCOUNT_USER => $request->input(Account::ACCOUNT_USER));
+        
+            $array_update = array(Account::ACCOUNT_DIS => 0);
+        
+            DB::table(Account::TABLE_ACCOUNT)->where($array_where)->update($array_update);
+            
+            return view('show.showDisable')->with('message','unclock successfuly');
+        }
+    }
+    
+    
+    
+    
+    
+}
