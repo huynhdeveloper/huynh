4623ed8da757a6ffe1c95830cbbeae79807ac5e3
huynhcntt2012 <huynhcntt2012@gmail.com>
2016-10-15 23:26:31 +0700

    clean file temp

--- public/tempData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(isset($_POST['id'])){
-    session_start();
-    $_SESSION['test'] = $_POST['id'];
-   // echo $_SESSION['test'];
-}
-
-?>
--- resources/views/chatonline/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    
-    foreach ($name as $subarray) {
-        $result += $subarray;
-    }
-    var_dump($result);
-
-?>
